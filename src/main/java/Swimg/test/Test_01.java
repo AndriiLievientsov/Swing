12e40dec4b63ae9c30133b7a91ec6c2f5e069e68
Apafios <andreiilevencov@gmail.com>
2023-06-17 18:32:14 +0300

    test01

--- src/main/java/Swimg/test/Test_01.java
+++ src/main/java/Swimg/test/Test_01.java
@@ -8,8 +8,8 @@ public class Test_01 {
         JFrame jFrame = new JFrame() {};
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(500,300);
-        jFrame.setLocation(900,500);
+//        jFrame.setSize(500,300);
+//        jFrame.setLocation(900,500);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
         jFrame.setBounds(dimension.width/2 - 250,dimension.height/2 - 150,500,300);
