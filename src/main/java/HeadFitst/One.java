18f35069977c8bd4fab6acfee3aade70a4540a35
Apafios <andreiilevencov@gmail.com>
2023-06-17 18:27:26 +0300

    test01

---
+++ src/main/java/HeadFitst/One.java
@@ -0,0 +1,31 @@
+package HeadFitst;
+
+public class One {
+    public static void main(String[] args) {
+        int x  = 0;
+        while ( x < 4 ) {
+            if (x < 1) {
+                System.out.print("a ");
+            }
+            if (x < 1){
+                System.out.print("noise");
+            }
+            if (x == 1) {
+                System.out.print("annoys");
+            }
+            if (x > 1){
+                System.out.print("an");
+                System.out.print(" oyster");
+                x = x + 2;
+            }
+            System.out.println("");
+            x = x + 1;
+
+
+
+        }//while and
+
+
+
+    }
+}
