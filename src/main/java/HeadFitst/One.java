e9ad7a500d32b2541f0c604b2b061dc8fb87274d
Apafios <andreiilevencov@gmail.com>
2023-06-26 20:34:39 +0300

    работа с программой. учебная

--- src/main/java/HeadFitst/One.java
+++ src/main/java/HeadFitst/One.java
@@ -1,32 +1,46 @@
 package HeadFitst;
 
-class Movie {
-    String title;
-    String genre;
-    int rating;
-    void playIt(){
-        System.out.println("Проигрывание фильма");
-    }
-}
-
-
-
 public class One {
     public static void main(String[] args) {
-        Movie one = new Movie();
-        one.title ="Как Прогореть на Акциях";
-        one.genre = "Трагедия";
-        one.rating = -2;
-        Movie two = new Movie();
-        two.title ="Потеряный офис";
-        two.genre = "Комедия";
-        two.rating = 5;
-        two.playIt();
-        Movie three = new Movie();
-        three.title = "Байт-Клуб";
-        three.genre = "Трагедия но в целом веселая";
-        three.rating = 127;
+//        class TapeDeck {
+//            boolean canRecord = false;
+//            void playTape() {
+//                System.out.println("пленка проигрывается");
+//            }
+//
+//            void recordTape() {
+//                System.out.println("идет запись на пленку");
+//            }
+//        }
+//
+//        TapeDeck t = new TapeDeck();
+//        t.canRecord = true;
+//        t.playTape();
+//        if (t.canRecord == true){
+//            t.recordTape();
+//        }
 
+        class DrumKit {
+            boolean topHat = true;
+            boolean snare = true;
+
+            void playSnare() {
+                System.out.println("бах бах ба-бах");
+            }
+
+            void playTopHat () {
+                System.out.println("динь динь ди-динь");
+            }
+        }
+
+
+            DrumKit d = new DrumKit();
+            d.playSnare();
+            d.snare = false;
+            d.playTopHat();
+           if (d.snare == true) {
+               d.playSnare();
+           }
 
 
 
