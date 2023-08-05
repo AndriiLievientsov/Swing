00053fcee227f80db21c773bec8ed07b0c27f7a0
Apafios <andreiilevencov@gmail.com>
2023-08-05 15:19:16 +0300

    суббота теория + практикум

---
+++ src/main/java/HeadFitst/SiteGame/SimpleDotCom.java
@@ -0,0 +1,28 @@
+package HeadFitst.SiteGame;
+
+public class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+    public void setLocationCells (int[] locs) {
+        locationCells = locs;
+    }
+
+    public String checkYourself (String stringGess) {
+        int guess = Integer.parseInt(stringGess);
+        String result  = "Мимо";
+        for (int cell : locationCells ) {
+            if (guess == cell) {
+                result = "Попал";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length){
+            result = "Потопил";
+        }
+        System.out.println(result);
+        return  result;
+
+    }
+
+}
