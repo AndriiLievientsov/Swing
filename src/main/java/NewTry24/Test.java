ba33da37cc20a3db684f223310b433d088c0029c
Apafios <andreiilevencov@gmail.com>
2024-03-05 08:28:11 +0200

    Вторник хф

---
+++ src/main/java/NewTry24/Test.java
@@ -0,0 +1,83 @@
+package NewTry24;
+
+import javax.swing.*;
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class Test {
+    public static void main(String[] args) {
+//        Scanner wow = new Scanner(System.in);
+//
+//        int weightOfAPerson;
+//        int elevatorWeightLimit;
+//        int numberOfPeople;
+//        System.out.print("Введи ограничение Лифта: ");
+//        int trueNumberOfPersonInelevator = wow.nextInt();
+//
+//        weightOfAPerson = 68;
+//        elevatorWeightLimit = 635;
+//        numberOfPeople = elevatorWeightLimit/weightOfAPerson;
+//        System.out.println(numberOfPeople);
+//        if (numberOfPeople <= trueNumberOfPersonInelevator) {
+//            System.out.println("Все члены семейсва Смитов влезли в лифт");
+//        }else {
+//            System.out.println("Сорян Голяк");
+//        }
+//---------------------------------------------------------------
+//        System.out.println("Введите пароль: ");
+//
+//        Scanner keyboard = new Scanner(System.in);
+//        String password = keyboard.next();
+//
+//        System.out.println("Вы ввули >>" + password + "<<");
+//        System.out.println();
+//
+//        if (password == "swordfish") {
+//            System.out.println("Введенный пароль ");
+//            System.out.println("совпадает с хранящимся ");
+//            System.out.println("паролем. Поздравляем, ");
+//            System.out.println("вы хаке. ");
+//        }else {
+//            System.out.println("Введенный пароль ");
+//            System.out.println("несовпадает с ");
+//            System.out.println("хранящимся паролем.");
+//            System.out.println("Вам просто не повезло.");
+//        }
+//        System.out.println();
+//
+//        if (password.equals("swordfish")) {//вот так СРАВНИВАТЬ СТРОКИ,правильней чем-  ==
+//            System.out.println("Введенный пароль ");
+//            System.out.println("правильный! ");
+//            System.out.println("Можете войти ");
+//            System.out.println("в систему.");
+//        }else {
+//            System.out.println("Введенный пароль ");
+//            System.out.println("неправильный! ");
+//            System.out.println("Попытайся ");
+//            System.out.println("еще раз.. ");
+//        }
+//        keyboard.close();
+//---------------------------------------------------------------
+//        String username = JOptionPane.showInputDialog("Имя пользователя");
+//        String password = JOptionPane.showInputDialog("Пароль: ");
+//        int numberOfCows = Integer.parseInt(JOptionPane.showInputDialog("Че почем??"));
+//        double fractionOfHolsteins = Double.parseDouble(JOptionPane.showInputDialog("Голшит: "));
+//
+//        if(username != null && password != null &&
+//                (
+//                (username.equals ("ЬЬurd") && password.equals ("swordfish")) ||
+//                (username.equals ( "hritter" ) && password.equals ("preakston"))
+//                )
+//        )
+//        {
+//            JOptionPane.showMessageDialog(null, "Вы допушены в систему.");
+//        }else {
+//            JOptionPane.showMessageDialog(null, "Попытайтесь еще раз..");
+//        }
+//---------------------------------------------------------------
+
+
+    }
+}
